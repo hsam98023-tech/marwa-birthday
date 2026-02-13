f19e610443452bf2d7b9a6b20a813dd39bb723c4
hsam98023-tech <hsam98023@gmail.com>
2026-02-13 22:23:00 +0100

    Initialize Supabase client with configuration

---
+++ SupabaseClient.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-client'
+
+export const supabase = createClient(
+  'https://wzlkkbitespgurpnçamy.supabase.co',
+  'Sb_publishable_i8oa1sU6vVZCncAv6LlNNA_0UM69X8I'
+)
