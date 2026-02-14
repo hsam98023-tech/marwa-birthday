9a1a0281e95f2180c64c3fafe8442d5ee62c37ea
hsam98023-tech <hsam98023@gmail.com>
2026-02-14 11:21:41 +0100

    Update Supabase client URL and key

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://wzlkkbitespgurpnçamy.supabase.co',
-  'Sb_publishable_i8oa1sU6vVZCncAv6LlNNA_0UM69X8I'
+  'https://wzlkkbitespgurpncamy.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6bGtrYml0ZXNwZ3VycG5jYW15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5MjgzODEsImV4cCI6MjA4NjUwNDM4MX0.xSz72jXv7QWckfGrvc5GjpQjXylFupVsGX4mpuqu7Cc'
 )
